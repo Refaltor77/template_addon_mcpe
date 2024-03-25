d8fa5dc5f052ab080f5a8183f7a1782702e9bb38
Refaltor77 <elysiomartinspro@gmail.com>
2024-03-25 02:11:54 +0100

    feat: add Database (by Jayly)

--- packs/BP/scripts_dev/events/onJoin.ts
+++ packs/BP/scripts_dev/events/onJoin.ts
@@ -1,16 +1,22 @@
 import Listener from "../framework/events/Listener";
 import PlayerJoinEvent from "../framework/events/types/player/PlayerJoinEvent";
 import Loader from "../Loader";
+import {Database} from "../framework/database/Database";
 
 export class onJoin extends Listener
 {
     onEvent(event: PlayerJoinEvent, loader: Loader): void
     {
+        const database = new Database("test", true);
         const player = event.getPlayer();
 
+        let join: number = 0;
+        if (database.has(player.id + ":join")) join = <number>database.get(player.id + ":join");
+        join++;
+        database.set(player.id + ":join", join);
         if (event.hasPlayedBefore())
         {
-            player.sendMessage("You have played before !");
+            player.sendMessage("You have played before ! Join : " + join);
         }
         else
         {
