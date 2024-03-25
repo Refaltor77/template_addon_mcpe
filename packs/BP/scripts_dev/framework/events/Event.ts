7cf371c5f8dee353e8b5b5c8e80ecae362f55d21
Refaltor77 <elysiomartinspro@gmail.com>
2024-03-25 15:52:34 +0100

    feat: add headers

--- packs/BP/scripts_dev/framework/events/Event.ts
+++ packs/BP/scripts_dev/framework/events/Event.ts
@@ -1,3 +1,22 @@
+/*
+ *
+ *  _____                   _       _     _
+ * | ____|__ _ ___ _   _   / \   __| | __| | ___  _ __
+ * |  _| / _` / __| | | | / _ \ / _` |/ _` |/ _ \| '_ \
+ * | |__| (_| \__ \ |_| |/ ___ \ (_| | (_| | (_) | | | |
+ * |_____\__,_|___/\__, /_/   \_\__,_|\__,_|\___/|_| |_|
+ *                 |___/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author Refaltor77
+ *
+ *
+ */
+
 import {HandlerListManager} from "./HandlerListManager";
 import Loader from "../../Loader";
 
--- packs/BP/scripts_dev/framework/helpers/BlockHelper.ts
+++ packs/BP/scripts_dev/framework/helpers/BlockHelper.ts
@@ -1,6 +1,24 @@
+/*
+ *
+ *  _____                   _       _     _
+ * | ____|__ _ ___ _   _   / \   __| | __| | ___  _ __
+ * |  _| / _` / __| | | | / _ \ / _` |/ _` |/ _ \| '_ \
+ * | |__| (_| \__ \ |_| |/ ___ \ (_| | (_| | (_) | | | |
+ * |_____\__,_|___/\__, /_/   \_\__,_|\__,_|\___/|_| |_|
+ *                 |___/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author Refaltor77
+ *
+ *
+ */
+
 import {Block} from "@minecraft/server";
 import {MinecraftBlockTypes} from "./Vanilla";
-import Loader from "../../Loader";
 
 export class BlockHelper
 {
