35381e7685495db88a73039fa8202f8e0df80383
Refaltor77 <elysiomartinspro@gmail.com>
2024-03-27 17:24:35 +0100

    feat: add Health function in PPlayer object

--- packs/BP/scripts_dev/events/onBreak.ts
+++ packs/BP/scripts_dev/events/onBreak.ts
@@ -2,12 +2,12 @@ import Listener from "../framework/events/Listener";
 import BlockBreakEvent from "../framework/events/types/block/BlockBreakEvent";
 import Loader from "../Loader";
 import {PPlayer} from "../framework/player/PPlayer";
+import EntitySpawnEvent from "../framework/events/types/entity/EntitySpawnEvent";
 
 export class onBreak extends Listener
 {
-    onEvent(event: BlockBreakEvent, loader: Loader)
+    onEvent(event: EntitySpawnEvent, loader: Loader)
     {
-        const player: PPlayer = event.getPlayer();
-        player.getInventory().clearAll();
+
     }
 }
--- packs/BP/scripts_dev/framework/player/PPlayer.ts
+++ packs/BP/scripts_dev/framework/player/PPlayer.ts
@@ -1,18 +1,21 @@
-import {Player} from "@minecraft/server";
+import {EntityHealableComponent, EntityHealthComponent, Player, system} from "@minecraft/server";
 import {PlayerInventory} from "../inventory/PlayerInventory";
 import {PlayerArmorInventory} from "../inventory/PlayerArmorInventory";
+import {Database} from "../database/Database";
 
 export class PPlayer
 {
     private readonly realPlayer: Player;
     private readonly inventory: PlayerInventory;
     private readonly armorInventory: PlayerArmorInventory;
+    private readonly databasePlayer: Database;
 
     constructor(realPlayer: Player)
     {
         this.realPlayer = realPlayer;
         this.inventory = new PlayerInventory(this);
         this.armorInventory = new PlayerArmorInventory(this);
+        this.databasePlayer = new Database(this.getRealPlayer().name + ":data_player");
     }
 
     public getRealPlayer(): Player
@@ -35,5 +38,87 @@ export class PPlayer
         const realPlayer: Player = this.getRealPlayer();
         if (!realPlayer.isValid()) return false;
         return this.getArmorInventory().inventoryIsValid();
+    }
+
+    public getDatabasePlayer(): Database
+    {
+        return this.databasePlayer;
+    }
+
+
+    // database system (data was save in the world)
+    public setData(key: string, value: string|number|boolean): void
+    {
+        this.getDatabasePlayer().set(key, value);
+    }
+
+    public getData(key: string): string|number|boolean
+    {
+        return this.getDatabasePlayer().get(key);
+    }
+
+    public hasData(key: string): boolean
+    {
+        return this.getDatabasePlayer().has(key);
+    }
+
+    public getHealth(): number
+    {
+        const component = this.getHealthComponent();
+        if (component instanceof EntityHealthComponent)
+        {
+            return component.currentValue;
+        }
+        return 0;
+    }
+
+    public getMaxHealth(): number
+    {
+        const component = this.getHealthComponent();
+        if (component instanceof EntityHealthComponent)
+        {
+            return component.effectiveMax;
+        }
+        return 0;
+    }
+
+    public setHealth(value: number): boolean
+    {
+        const component = this.getHealthComponent();
+        if (component instanceof EntityHealthComponent)
+        {
+            system.run(() =>
+            {
+                component.setCurrentValue(value);
+            });
+            return true;
+        }
+        return false;
+    }
+
+    public resetHealth(): boolean
+    {
+        const component = this.getHealthComponent();
+        if (component instanceof EntityHealthComponent)
+        {
+            system.run(() =>
+            {
+                component.resetToMaxValue();
+            });
+            return true;
+        }
+        return false;
+    }
+
+
+    // components for this class
+    private getHealthComponent(): EntityHealthComponent|null
+    {
+        const component = this.getRealPlayer().getComponent(EntityHealthComponent.componentId);
+        if (component instanceof EntityHealthComponent)
+        {
+            return component;
+        }
+        return null;
     }
 }
